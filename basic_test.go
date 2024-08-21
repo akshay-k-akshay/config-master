98f154dcc0282622bf4dcea56f72214d0c89656e
Akshay K <akshayk.info01@gmail.com>
2024-08-22 00:12:29 +0530

    set config logic updated

---
+++ basic_test.go
@@ -0,0 +1,167 @@
+package configmaster
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestWithBasicFlatJson(t *testing.T) {
+	directMap := map[string]interface{}{
+		"foo": "bar",
+	}
+	config, err := NewConfig(directMap)
+	if err != nil {
+		t.Fatalf(`NewConfig() = %v, want nil`, err)
+	}
+	value := config.Get("foo")
+	want := `bar`
+	if value != want {
+		t.Fatalf(`config.Get("foo") should be "%v", got "%v"`, want, value)
+	}
+}
+
+func TestWithBasicJson(t *testing.T) {
+	directMap := map[string]interface{}{
+		"foo": map[string]interface{}{
+			"default": "bar",
+			"env":     "FOO",
+		},
+	}
+	config, err := NewConfig(directMap)
+	if err != nil {
+		t.Fatalf(`NewConfig() = %v, want nil`, err)
+	}
+	value := config.Get("foo")
+	want := `bar`
+	if value != want {
+		t.Fatalf(`config.Get("foo") should be "%v", got "%v"`, want, value)
+	}
+}
+
+func TestWithValueFromEnv(t *testing.T) {
+	directMap := map[string]interface{}{
+		"foo": map[string]interface{}{
+			"default": "bar",
+			"env":     "FOO_ENV",
+		},
+	}
+	// set Env VAlue
+	want := "bar from env"
+	os.Setenv("FOO_ENV", want)
+	config, err := NewConfig(directMap)
+	if err != nil {
+		t.Fatalf(`NewConfig() = %v, want nil`, err)
+	}
+	value := config.Get("foo")
+	if value != want {
+		t.Fatalf(`config.Get("foo") should be "%v", got "%v"`, want, value)
+	}
+}
+
+func TestWithValueIsInFormat(t *testing.T) {
+	directMap := map[string]interface{}{
+		"foo": map[string]interface{}{
+			"format":  []interface{}{"bar", "baz", "foo"},
+			"default": "bar test",
+			"env":     "FOO",
+		},
+	}
+	// set Env VAlue
+	config, err := NewConfig(directMap)
+	if err == nil {
+		t.Fatalf(`should throw error if value is not in format %v`, err)
+	}
+
+	if config != nil {
+		t.Fatalf("should not return config as value is not in format")
+	}
+}
+
+func TestWithEnvValueIsInFormat(t *testing.T) {
+	directMap := map[string]interface{}{
+		"foo": map[string]interface{}{
+			"format":  []interface{}{"bar", "baz", "foo"},
+			"default": "bar",
+			"env":     "FOO_ENV",
+		},
+	}
+	// set Env VAlue
+	os.Setenv("FOO_ENV", "bar test")
+	config, err := NewConfig(directMap)
+	if err == nil {
+		t.Fatalf(`should throw error if value is not in format %v`, err)
+	}
+
+	if config != nil {
+		t.Fatalf("should not return config as value is not in format")
+	}
+}
+
+func TestWithNestedConfig(t *testing.T) {
+	directMap := map[string]interface{}{
+		"foo": map[string]interface{}{
+			"bar": map[string]interface{}{
+				"format":  []interface{}{"bar", "baz", "foo"},
+				"default": "bar",
+				"env":     "BAR",
+			},
+			"quux": map[string]interface{}{
+				"format":  []interface{}{"bar", "baz", "foo"},
+				"default": "baz",
+				"env":     "QUUX",
+			},
+		},
+	}
+	config, err := NewConfig(directMap)
+
+	if err != nil {
+		t.Fatalf(`should throw error if value is not in format %v`, err)
+	}
+
+	value := config.Get("foo")
+	want := map[string]interface{}{
+		"bar":  "bar",
+		"quux": "baz",
+	}
+	if !reflect.DeepEqual(value, want) {
+		t.Fatalf(`config.Get("foo") should be "%v", got "%v"`, want, value)
+	}
+}
+
+func TestGetNestedKey(t *testing.T) {
+	directMap := map[string]interface{}{
+		"foo": map[string]interface{}{
+			"bar": map[string]interface{}{
+				"format":  []interface{}{"bar", "baz", "foo"},
+				"default": "baz",
+				"env":     "QUUX",
+			},
+		},
+	}
+	config, err := NewConfig(directMap)
+
+	if err != nil {
+		t.Fatalf(`should throw error if value is not in format %v`, err)
+	}
+
+	value := config.Get("foo.bar")
+
+	want := "baz"
+	if value != want {
+		t.Fatalf(`config.Get("foo") should be "%v", got "%v"`, want, value)
+	}
+}
+
+func TestNewConfigWithJsonFile(t *testing.T) {
+	config, err := NewConfig("./config.json")
+	if err != nil {
+		t.Fatalf(`should throw error if value is not in format %v`, err)
+	}
+	value := config.Get("foo")
+
+	want := "bar"
+	if value != want {
+		t.Fatalf(`config.Get("foo") should be "%v", got "%v"`, want, value)
+	}
+}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
-package config
+package configmaster
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"reflect"
+	"strings"
 )
 
 // Config represents the configuration and provides methods to access it
@@ -37,66 +40,110 @@ func NewConfig(input interface{}) (*Config, error) {
 	} else if configMap, ok := input.(map[string]interface{}); ok {
 		// If input is already a map
 		config = configMap
-
 	} else {
 		return nil, fmt.Errorf("unsupported input type: %T", input)
 	}
 
 	cfg := &Config{data: config}
-	cfg.ProcessEnv() // Automatically process environment variables
+	err := cfg.ProcessEnv() // Automatically process environment variables
+	if err != nil {
+		return nil, fmt.Errorf("[Config-Master]: %w", err)
+	}
 	return cfg, nil
 }
 
 // Get retrieves a value from the configuration by key
 func (c *Config) Get(key string) interface{} {
+	if strings.Contains(key, ".") {
+		ar := strings.Split(key, ".")
+		var o interface{}
+		o = c.data
+		for _, k := range ar {
+			o = o.(map[string]interface{})[k]
+			if o == nil {
+				return nil
+			}
+		}
+		return o
+	}
 	return c.data[key]
 }
 
 // ProcessEnv replaces values associated with the 'env' key with environment variable values or fallback to default
-func (c *Config) ProcessEnv() {
-	c.processEnvRecursively(c.data)
+func (c *Config) ProcessEnv() error {
+	err := c.processEnvRecursively(c.data)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-// processEnvRecursively iterates over the config map and replaces 'env' key values with environment variable values or default
-func (c *Config) processEnvRecursively(config map[string]interface{}) {
-	for key, value := range config {
-		switch v := value.(type) {
-		case map[string]interface{}:
-			// Process 'env' key if present
-			if envKey, exists := v["env"].(string); exists {
-				// Resolve environment variable
-				if envValue, ok := os.LookupEnv(envKey); ok {
-					config[key] = envValue
-				} else {
-					// If environment variable is not set, fall back to default
-					if defaultValue, defaultExists := v["default"].(string); defaultExists {
-						config[key] = defaultValue
-					} else {
-						config[key] = "" // or handle the missing default value case
-					}
-				}
-			} else {
-				// Recursively process nested maps
-				c.processEnvRecursively(v)
-			}
+func contains[T comparable](slice []T, value T) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+func setValue(envKey string, v map[string]interface{}) (interface{}, error) {
+	var value interface{}
+	if envValue, ok := os.LookupEnv(envKey); ok {
+		value = envValue
+	} else {
+		if defaultValue, defaultExists := v["default"].(string); defaultExists {
+			value = defaultValue
+		} else {
+			value = ""
+		}
+	}
+	valueType := reflect.TypeOf(value)
+	if format, formatExists := v["format"]; formatExists {
+		switch v := format.(type) {
 		case []interface{}:
-			for i, item := range v {
-				if subMap, ok := item.(map[string]interface{}); ok {
-					c.processEnvRecursively(subMap)
-					// Handle 'env' key at the array item level
-					if envKey, exists := subMap["env"].(string); exists {
-						if envValue, ok := os.LookupEnv(envKey); ok {
-							v[i] = envValue
-						} else {
-							if defaultValue, defaultExists := subMap["default"].(string); defaultExists {
-								v[i] = defaultValue
-							} else {
-								v[i] = "" // or handle the missing default value case
-							}
-						}
-					}
-				}
+			if !contains(v, value) {
+				return 0, errors.New("value is not in format")
+			}
+		case string:
+			if valueType != reflect.TypeOf("") {
+				return 0, errors.New("value should be string")
+			}
+		case bool:
+			if valueType != reflect.TypeOf(true) && valueType != reflect.TypeOf(false) {
+				return 0, errors.New("value should be bool")
+			}
+		case float64:
+			if valueType != reflect.TypeOf(float64(0)) {
+				return 0, errors.New("value should be float64")
+			}
+		case int:
+			if valueType != reflect.TypeOf(int(0)) {
+				return 0, errors.New("value should be int")
 			}
 		}
 	}
+	return value, nil
+}
+
+// processEnvRecursively iterates over the config map and replaces 'env' key values with environment variable values or default
+func (c *Config) processEnvRecursively(config map[string]interface{}) error {
+	for key, value := range config {
+		switch v := value.(type) {
+		case map[string]interface{}:
+			if envKey, exists := v["env"].(string); exists {
+				value, err := setValue(envKey, v)
+				if err != nil {
+					return err
+				}
+				config[key] = value
+			} else {
+				c.processEnvRecursively(v)
+			}
+		default:
+			config[key] = value
+		}
+	}
+
+	return nil
 }
